e17e1b965e74c6e8b32ec73e0919d8b01d35db13
SamuCasta <scastanomi@unal.edu.co>
2025-09-27 11:17:12 -0500

    try

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -33,9 +33,9 @@ def load_best_estimator():
     import os
     import pickle
 
-    if not os.path.exists("homework\estimator.pickle"):
+    if not os.path.exists("homework/estimator.pickle"):
         return None
-    with open("homework\estimator.pickle", "rb") as file:
+    with open("homework/estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
 
     return estimator
